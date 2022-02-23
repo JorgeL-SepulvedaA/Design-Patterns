73b1123a25bd9de88ca0b73867332bb8a85bed00
Jorge Luis Sepúlveda Abreu <202010714@itla.edu.do>
2022-02-23 17:19:42 -0400

    Create ListsRepository.cs

---
+++ Pick-UP/LogicLayer/ListsRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicLayer
+{
+    public sealed class ListsRepository
+    {
+        public ListsRepository() { }
+
+        public static ListsRepository Repository { get; set; } = new ListsRepository();
+
+        public List<CategoriesModel> Categories { get; set; } = new List<CategoriesModel>
+        {
+            new CategoriesModel
+            {
+                Name = "Vegetales"
+            },
+
+            new CategoriesModel
+            {
+                Name = "Frutos"
+            },
+
+            new CategoriesModel
+            {
+                Name = "Lacteos"
+            }
+        };
+
+        public List<ProductsModel> Products { get; set; } = new List<ProductsModel>
+        {
+            new ProductsModel
+            {
+                Category = Repository.Categories[0].Name,
+                Product_Name = "Verdura",
+                Product_Price = 08.94
+            },
+            new ProductsModel
+            {
+                Category = Repository.Categories[1].Name,
+                Product_Name = "Banana",
+                Product_Price = 07.24
+            },
+            new ProductsModel
+            {
+                Category = Repository.Categories[2].Name,
+                Product_Name = "Leche de vaca",
+                Product_Price = 64.58
+            }
+        };
+    }
+}
