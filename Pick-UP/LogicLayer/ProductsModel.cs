523893c0d27834b4efa95aab563647fb62a5715f
Jorge Luis Sepúlveda Abreu <202010714@itla.edu.do>
2022-02-23 16:40:30 -0400

    Update ProductsModel.cs

--- Pick-UP/LogicLayer/ProductsModel.cs
+++ Pick-UP/LogicLayer/ProductsModel.cs
@@ -2,7 +2,10 @@
 
 namespace LogicLayer
 {
-    public   class ProductsModel
+    public class ProductsModel
     {
+        public string Category { get; set; }
+        public string Product_Name { get; set; }
+        public double Product_Price { get; set; }
     }
 }
