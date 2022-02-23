9cc9a2208e99951b96a65b5e6159bcff661f0670
Jorge Luis Sepúlveda Abreu <202010714@itla.edu.do>
2022-02-23 16:51:29 -0400

    Create CategoriesModel.cs

---
+++ Pick-UP/LogicLayer/CategoriesModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicLayer
+{
+    public class CategoriesModel
+    {
+        public string Name { get; set; }
+    }
+}
