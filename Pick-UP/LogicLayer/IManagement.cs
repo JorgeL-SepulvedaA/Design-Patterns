b5107869e1b49697fae48bbae6464192f96ee7b0
Jorge Luis Sepúlveda Abreu <202010714@itla.edu.do>
2022-02-23 21:19:46 -0400

    Create IManagement.cs

---
+++ Pick-UP/LogicLayer/IManagement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicLayer
+{
+    public interface IManagement
+    {
+        void Create(string[] vs);
+        void Update(string[] vs);
+        void Delete(string[] vs);
+        void Read(string[] vs);
+        void ReadAll(string[] vs);
+    }
+}
