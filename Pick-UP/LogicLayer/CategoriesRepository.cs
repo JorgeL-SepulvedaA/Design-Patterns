37be24ed2112dd1cbfa490197b654b274559530c
Jorge Luis Sepúlveda Abreu <202010714@itla.edu.do>
2022-02-23 21:17:06 -0400

    Create CategoriesRepository.cs

---
+++ Pick-UP/LogicLayer/CategoriesRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicLayer
+{
+    public class CategoriesRepository : IManagement
+    {
+        public void Create(string[] vs)
+        {
+
+        }
+        public void Update(string[] vs)
+        {
+
+        }
+        public void Delete(string[] vs)
+        {
+
+        }
+        public void Read(string[] vs)
+        {
+
+        }
+        public void ReadAll(string[] vs)
+        {
+
+        }
+    }
+}
