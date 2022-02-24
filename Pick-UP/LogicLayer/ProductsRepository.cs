6dc85318d8a0f05eb8ca06ce120ccae5955a3fb9
Jorge Luis Sepúlveda Abreu <202010714@itla.edu.do>
2022-02-23 21:22:39 -0400

    Create ProductsRepository.cs

---
+++ Pick-UP/LogicLayer/ProductsRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicLayer
+{
+    public class ProductsRepository : IManagement
+    {
+        public void Create(string category, string name, string price)
+        {
+            
+        }
+        public void Update(string[] vs)
+        {
+
+        }
+        public void Delete(string[] vs)
+        {
+
+        }
+        public void Read(string[] vs)
+        {
+
+        }
+        public void ReadAll(string[] vs)
+        {
+
+        }
+    }
+}
